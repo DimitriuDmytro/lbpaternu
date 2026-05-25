4ed286f541499fa5a319c4febe20ca102fa40059
DimitriuDmytro <dimitriudima28@gmail.com>
2026-05-26 02:54:27 +0300

    feat: додано лабораторну роботу №6

---
+++ laba6/laba6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using lb5;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var nums = new List<int> { 1, 2, 3, 4, 10, 8, 5 };
+var words = new List<string> { "BMW", "Audi", "Mercedes", "X6" };
+
+Console.WriteLine("--- РЕЗУЛЬТАТИ ЛБ №5 ---");
+Console.WriteLine($"3. Алфавіт: {string.Join(", ", LambdaTasks.SortAlphabetically(words))}");
+
+Console.WriteLine("--- РЕЗУЛЬТАТИ ЛБ №6 (Патерни) ---");
+
+var sorted = PatternTasks.ApplyStrategy(words, w => w.Length);
+Console.WriteLine($"Стратегія (за довжиною): {string.Join(", ", sorted)}");
+
+var bmwFactory = PatternTasks.CreateCarFactory("BMW");
+Console.WriteLine($"Фабрика: {bmwFactory("X6")}");
+
+PatternTasks.ExecuteWithLog("Аналіз", () => {
+    Console.WriteLine("Тут виконується бізнес-логіка...");
+});
